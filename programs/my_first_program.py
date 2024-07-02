00f271eac343397b9aacbca4276a8c494185be7c
DEEPAKKAPOOR-1 <deepakkapoor.work@gmail.com>
2024-07-02 07:05:50 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,12 +1,18 @@
 from nada_dsl import *
 
-
-def nada_main():
+def new_nada_program():
     party1 = Party(name="Party1")
+    party2 = Party(name="Party2")  # Adding another party
+
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    my_int2 = SecretInteger(Input(name="my_int2", party=party2))  # Using different parties
 
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
+    # Computation: Here you can perform operations on my_int1 and my_int2
+    result = my_int1 + my_int2  # Example addition operation
 
-    return [Output(my_int1, "my_output", party1)]
+    return [Output(result, "my_output", party1)]  # Returning the result as an output
+
+# Example usage
+if __name__ == "__main__":
+    program = new_nada_program()
+    print(program)
